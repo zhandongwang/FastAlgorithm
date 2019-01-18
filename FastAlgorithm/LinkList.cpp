db31dafdc60a902e34d4f46d8911fec576252d87
fengli <fengli@2dfire.com>
2019-01-18 16:45:33 +0800

    feat:有序链表合并

--- FastAlgorithm/LinkList.cpp
+++ FastAlgorithm/LinkList.cpp
@@ -114,3 +114,25 @@ ListNode *FindKthToTail(ListNode *pListHead, unsigned int k) {
     }
     return pSlow;
 }
+
+//面试题17 合并两个递增有序的链表
+//思路:得到值较小的头节点并把它链接到已合并链表的尾部
+ListNode* Merge(ListNode *pHead1, ListNode *pHead2) {
+    if (pHead1 == NULL) {
+        return pHead2;
+    }
+    if (pHead2 == NULL) {
+        return pHead1;
+    }
+    
+    ListNode *pMergedHead = NULL;
+    if (pHead1->m_nValue < pHead2->m_nValue) {
+        pMergedHead = pHead1;
+        pMergedHead->m_pNext = Merge(pHead1->m_pNext, pHead2);
+    } else {
+        pMergedHead = pHead2;
+        pMergedHead->m_pNext = Merge(pHead1, pHead2->m_pNext);
+    }
+    return pMergedHead;
+}
+
